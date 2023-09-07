c5750e1a7da87a3e84a604e10306af82f92ad08e
Vishal Singh <singhvishal1821@gmail.com>
2023-09-07 11:30:13 +0530

    changes done

--- 01_basics/08_dates_and_time.js
+++ 01_basics/08_dates_and_time.js
@@ -15,6 +15,6 @@ let myCreatedDate = new Date(2023 , 12, 11 , 5 , 45)
 // console.log(myCreatedDate.toLocaleString());
 
 let myCreatedDate2 = new Date(2023 , 12 , 11)
-// console.log(myCreatedDate2.toLocaleString());
+console.log(myCreatedDate2.toLocaleString());
 // console.log(myCreatedDate2.getTime());   //will return the ms value to created date since 1971.
 // console.log(Date.now());     // returns the current timestamp in milliseconds since 1971.
--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -3,6 +3,8 @@
 let arr = ["vishal", 28, true, { name: "vishal" }];
 // console.log(arr);
 
+console.log(arr[3]);
+
 arr.push(5);
 arr.push(8);
 arr.pop();
@@ -22,25 +24,25 @@ let arr3 = [2, 3, 5, 7, 8, 9];
 
 // console.log("A", arr3);
 
-// let narr3 = arr3.slice(1, 4);
+let narr3 = arr3.slice(1, 4);
 
 // console.log("B", narr3);
-console.log("A", arr3);
+// console.log("A", arr3);
 
 let narr4 = arr3.splice(0, 3); //changes the contents of an array by removing or
 //  replacing existing elements and/or adding new elements
 
-console.log("C", narr4);
-console.log("A", arr3);
+// console.log("C", narr4);
+// console.log("A", arr3);
 
-/*
-Adding elements through splice in array
 
-console.log(arr);
+// Adding elements through splice in array
 
-arr.splice(0,3,10,20,30);  (start idx, end idx , items to be added )
+// console.log(arr);
 
-console.log(arr);
-*/
+arr.splice(0,3,10,20,30); //  (start idx, end idx , items to be added )
+
+// console.log(arr);
+
 
 // 
